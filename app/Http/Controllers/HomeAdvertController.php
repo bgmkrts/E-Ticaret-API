1cbe34c36c51c0ec72bf8264fd941098b03e8af7
bgmkrts <34838510+bgmkrts@users.noreply.github.com>
2019-12-03 11:35:22 +0300

    Ev ilanı
    ev ilanları oluşturuldu

---
+++ app/Http/Controllers/HomeAdvertController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\FurnitureAdvertModel;
+use App\Models\HomeAdvertModel;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;use Illuminate\Support\Facades\Validator;
+
+class HomeAdvertController extends Controller
+{
+    public function index(){
+
+        $home_adverts=HomeAdvertModel::with('User','Cities','Statu','Wage','Advert')->get();
+        return Response::json([
+            'result'=>'ok',
+            'data'=>$home_adverts,
+        ]);
+    }
+    public function create(Request $request){
+        $validator = Validator::make($request->all(), [
+            'cities_id'=>'',
+            'status_id'=>'required|int',
+            'explanation'=>'required',
+            'wages_id'=>'required|int',
+            'users_id'=>'',
+            'adverts_id'=>'required|int'
+            ]);
+
+        if ($validator->fails()) {
+            $errors = array();
+            foreach ($validator->errors()->messages() as $key => $value) {
+                $errors[] = [
+                    'field' => $key,
+                    'message' => $value[0]
+                ];
+            }
+            return Response::json([
+                'result' => 'Error',
+                "code" => "10",
+                'errors' => $errors,
+            ]);
+        }
+        $user = auth('api')->user();
+        $home_adverts=new HomeAdvertModel();
+        $home_adverts->users_id=$user->id;
+        $home_adverts->cities_id=$request->cities_id;
+        $home_adverts->status_id=$request->status_id;
+        $home_adverts->explanation=$request->explanation;
+        $home_adverts->wages_id=$request->wages_id;
+        $home_adverts->adverts_id=$request->adverts_id;
+        $home_adverts->save();
+        return Response::json([
+            'message'=>'Home advert created',
+            'result'=>'ok'
+        ]);
+
+    }
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'cities_id'=>'',
+            'status_id'=>'required|int',
+            'explanation'=>'required',
+            'wages_id'=>'required|int',
+            'users_id'=>'',
+            'adverts_id'=>'required|int'
+        ]);
+        if ($validator->fails()) {
+            $errors = array();
+            foreach ($validator->errors()->messages() as $key => $value) {
+                $errors[] = [
+                    'field' => $key,
+                    'message' => $value[0]
+                ];
+            }
+            return Response::json([
+                'result' => 'Error',
+                "code" => "10",
+                'errors' => $errors,
+            ]);
+        }
+        $home_adverts = FurnitureAdvertModel::find($request->id);
+        $home_adverts->id=$request->id;
+        $home_adverts->cities_id=$request->cities_id;
+        $home_adverts->furniture_name=$request->explanation;
+        $home_adverts->adverts_id=$request->adverts_id;
+        $home_adverts->status_id=$request->status_id;
+        $home_adverts->wages_id=$request->wages_id;
+        $home_adverts->save();
+        return Response::json([
+            'result' => 'Home advert updated'
+        ]);
+    }
+}
--- app/Models/HomeAdvertModel.php
+++ app/Models/HomeAdvertModel.php
@@ -16,4 +16,19 @@ class HomeAdvertModel extends Model
         'users_id',
         'adverts_id'
     ];
+    public function User(){
+        return $this->belongsTo(UserModel::class,'users_id','id');
+    }
+    public function Cities(){
+        return $this->hasMany(CityModel::class,'id','cities_id');
+    }
+    public function Statu(){
+        return $this->belongsTo(StatuModel::class,'status_id','id');
+    }
+    public function Wage(){
+        return $this->belongsTo(WageModel::class,'wages_id','id');
+    }
+    public function Advert(){
+        return $this->belongsTo(AdvertModel::class,'adverts_id','id');
+    }
 }
